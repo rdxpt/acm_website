9ebdbeacbfeceef87095a8618716b0b09b215b6c
rdxpt <rdxpt@outlook.com>
2025-03-03 20:37:02 +0530

    minor

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  export default nextConfig;
-
-export const images = {
+  experimental: {
+    runtime: 'nodejs', // Ensures it's running on Node.js, not Edge runtime
+  },
+  output: 'export', // Keep this if you're deploying a static site
+  images: {
     unoptimized: true,
+  },
 };
+
+export default nextConfig;
